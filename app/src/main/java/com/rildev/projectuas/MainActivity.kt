1bc56769def0cd848f291e5aa46fbf800fa1ee97
cheryldanielaa <cheryldanielaaaaa@gmail.com>
2024-12-14 13:30:55 +0700

    gk bisa sign out sm e-sport mengsle

--- app/src/main/java/com/rildev/projectuas/MainActivity.kt
+++ app/src/main/java/com/rildev/projectuas/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.rildev.projectuas
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.snackbar.Snackbar
 import com.rildev.projectuas.databinding.ActivityMainBinding
+import com.rildev.projectuas.databinding.DrawerLayoutBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: DrawerLayoutBinding
     //deklarasi semua list yang dipake buat fragment itu disini
     //deklarasikan list dari cabang yang ada
 
@@ -26,8 +35,23 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         //deklarasikan view binding
-        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        //karena dia pake drawer maka bindingnya jadi di drawerlayout
+        binding = DrawerLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //set ada toolbar di atas
+        setSupportActionBar(binding.mainActivity.menuToolbar)
+
+        //tambahin hamburger wow
+        supportActionBar?.setDisplayHomeAsUpEnabled(false); //krn mau pke hamburger icon buat ke navbar makanya set false
+        //buat hubungin drawer ke playlist detailnya
+        var drawerToggle = ActionBarDrawerToggle(
+            this, binding.drawerLayout,
+            binding.mainActivity.menuToolbar, R.string.app_name, R.string.app_name
+        )
+        drawerToggle.isDrawerIndicatorEnabled = true
+        drawerToggle.syncState()
 
         //atur viewpager, masukin what we play ke dalem list, biar nnti dia
         //keintegrasi trs bs digeser"
@@ -40,26 +64,45 @@ class MainActivity : AppCompatActivity() {
         fragments.add(OurScheduleFragment())
         fragments.add(WhoWeAreFragment())
 
-        binding.viewPager.adapter = MyAdapter(this, fragments) //hubungin sm
+        binding.mainActivity.viewPager.adapter = MyAdapter(this, fragments) //hubungin sm
 
         //ini buat ngecode klo viewpagernya digeser, menu item yang dihover ikut geser
-        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+        binding.mainActivity.viewPager.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback() {
             //fungsi ini akan dijalankan setiap kali pagenya berubah
             override fun onPageSelected(position: Int) {
                 //ini buat ambil id dari page sama id dari navmenu
                 //jd klo kita geser pagenya, nav menunya jg berubah (yg kehover)
-                binding.bottomNav.selectedItemId =
-                    binding.bottomNav.menu.getItem(position).itemId
+                binding.mainActivity.bottomNav.selectedItemId =
+                    binding.mainActivity.bottomNav.menu.getItem(position).itemId
             }
         })
 
-        binding.bottomNav.setOnItemSelectedListener {
-            binding.viewPager.currentItem = when (it.itemId) {
+        binding.mainActivity.bottomNav.setOnItemSelectedListener {
+            binding.mainActivity.viewPager.currentItem = when (it.itemId) {
                 R.id.WhatWePlay -> 0 //klo index ke 0 di bottomNav=0, maka what we play
                 R.id.OurSchedule -> 1 //klo index ke 1 di bottomNav=1, maka  our schedule
                 R.id.WhoWeAre -> 2 //klo index ke 2 di bottomNav=2, maka who we are
                 else -> 0 // default to home
             }
+            true
+        }
+
+        //atur buat apa yang terjadi klo apply team dan sign out diklik
+        binding.navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                //klo klik apply team, buka page apply team activity
+                R.id.itemApplyTeam -> {
+                    val intent = Intent(this, ProposalListActivity::class.java)
+                    startActivity(intent)
+                }
+
+                R.id.itemSignOut -> {
+                    //GAK BISA SIGN OUT, SHARED PREFERENCES GAK KEHAPUS SELAMANYA
+                    Toast.makeText(this,"sedihhh",Toast.LENGTH_SHORT).show()
+                }
+            }
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
             true
         }
     }
--- app/src/main/java/com/rildev/projectuas/SignIn.kt
+++ app/src/main/java/com/rildev/projectuas/SignIn.kt
@@ -49,7 +49,8 @@ class SignIn : AppCompatActivity() {
             //klo usn sama password tdk kosong
             if (usn.isNotEmpty() && pass.isNotEmpty()) {
                 //urus ke database
-                val q = Volley.newRequestQueue(this) //karena layout sign in itu activity, maka pake this
+                val q =
+                    Volley.newRequestQueue(this) //karena layout sign in itu activity, maka pake this
                 //panggil url dimana apinya dibuat
                 val url = "https://ubaya.xyz/native/160422026/project/login.php"
                 //waktu login itu kita kirim data, jd hrus pake anonymous object
@@ -75,7 +76,8 @@ class SignIn : AppCompatActivity() {
 
                             //update shared preferencesnya >> dari semula false jadi true
                             //klo true maka update shared preferencesnya
-                            login_state = true //login statenya jadi true (buat simpen status loginnya)
+                            login_state =
+                                true //login statenya jadi true (buat simpen status loginnya)
                             val editor = sharedPreferences.edit()
                             editor.putBoolean("LOGIN_STATE", login_state)
                             editor.putInt("user_id", idmember)
@@ -121,75 +123,6 @@ class SignIn : AppCompatActivity() {
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
             finish() //biar gk bs ngeback
-        }
-
-        binding.buttonApply.setOnClickListener{
-            var usn = binding.txtUsername.text.toString()
-            var pass = binding.txtPassword.text.toString()
-            //klo usn sama password tdk kosong
-            if (usn.isNotEmpty() && pass.isNotEmpty()) {
-                //urus ke database
-                val q = Volley.newRequestQueue(this) //karena layout sign in itu activity, maka pake this
-                //panggil url dimana apinya dibuat
-                val url = "https://ubaya.xyz/native/160422026/project/login.php"
-                //waktu login itu kita kirim data, jd hrus pake anonymous object
-                val stringRequest = object : StringRequest(Request.Method.POST, url,
-                    //klo berhasil
-                    Response.Listener
-                    {
-                        //baca data dari json
-                        val obj = JSONObject(it)
-                        //klo resultnya OK
-                        if (obj.getString("result") == "OK") {
-                            //klo diliat dari json viewer, objek besarnya namanya data
-                            val data = obj.getJSONArray("data")
-                            //karena outputnya pasti cuman satu maka ambil index ke -
-                            val userJson = data.getJSONObject(0)
-                            val idmember = userJson.getInt("idmember")
-                            //untuk mendapatkan object user yang lagi login
-                            val sType = object : TypeToken<User>() {}.type
-                            //baca data user dari json
-                            user = Gson().fromJson<User>(userJson.toString(), sType)
-                            Log.d("apiresult", user.toString())
-                            Toast.makeText(this, "Selamat datang!", Toast.LENGTH_SHORT).show()
-
-                            //update shared preferencesnya >> dari semula false jadi true
-                            //klo true maka update shared preferencesnya
-                            login_state = true //login statenya jadi true (buat simpen status loginnya)
-                            val editor = sharedPreferences.edit()
-                            editor.putBoolean("LOGIN_STATE", login_state)
-                            editor.putInt("user_id", idmember)
-                            editor.apply()
-
-                            //klo benar maka muncul main activity sama bikin user shared preferences
-                            val intent = Intent(this, ApplyTeamActivity::class.java)
-                            startActivity(intent)
-                            finish() //biar gk bisa back lagi
-                        } else {
-                            Toast.makeText(
-                                this,
-                                "Maaf, username/password yang Anda masukkan salah!",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                    },
-                    Response.ErrorListener {
-                        Log.d("apiresult", it.message.toString())
-                    }) {
-                    override fun getParams(): MutableMap<String, String>? {
-                        val params = HashMap<String, String>()
-                        params["username"] = usn //kirim username yang diinput user
-                        params["password"] = pass //kirim password yang diinput user
-                        return params
-                    }
-                }
-                q.add(stringRequest)
-            } else {
-                Toast.makeText(this, "Silahkan isi username dan password Anda!", Toast.LENGTH_SHORT)
-                    .show()
-            }
-
-
         }
     }
 }
